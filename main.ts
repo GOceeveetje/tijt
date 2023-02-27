a27b7929abb32765a14f64039aa31f9ec5b5858d
GOceeveetje <125375573+GOceeveetje@users.noreply.github.com>
2023-02-27 22:46:18 +0100

    tijt bal naar computer getuur door gebruik te maken van serieel (einde project)

--- main.ts
+++ main.ts
@@ -1,15 +1,28 @@
 let tijt1 = 0
-let spaning1 = 0
-let toetand1 = 0
 let tijt2 = 0
 let tijt3 = 0
-let spaning2 = 0
-let toestan2 = 0
 let tijt4 = 0
 let tijt5 = 0
-let spaning3 = 0
-let toestand3 = 0
 let tijt6 = 0
+let spaning1 = 0
+let spaning2 = 0
+let spaning3 = 0
+let toetand1 = 0
+let toestan2 = 0
+let toestand3 = 0
+basic.forever(function () {
+    serial.writeNumber(tijt1 - tijt2)
+    serial.writeNumber(tijt3 - tijt4)
+    serial.writeNumber(tijt5 - tijt6)
+    serial.writeNumber(spaning1)
+    serial.writeNumber(spaning2)
+    serial.writeNumber(spaning3)
+    serial.redirect(
+    SerialPin.USB_TX,
+    SerialPin.USB_RX,
+    BaudRate.BaudRate115200
+    )
+})
 basic.forever(function () {
     if (pins.analogReadPin(AnalogPin.P0) > 512) {
         tijt1 = input.runningTime()
@@ -41,10 +54,4 @@ basic.forever(function () {
         spaning3 = pins.analogReadPin(AnalogPin.P2)
         toestand3 = 2
     }
-})
-basic.forever(function () {
-	
-})
-basic.forever(function () {
-	
 })
